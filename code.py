c5ead39f7cc8c800446e87bff37e66629076a867
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2023-01-09 19:31:40 +0000

    Added code.py

---
+++ code.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Created by: Peter Sobowale
+# Created on: January 2023
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage 
+
+
+def game_scene():
+    # this function is the main game screen
+
+    print("\n\n\n") # 3 blank lines
+    print("Hello, World(Peter)!") # what is going to be displayed
+
+    # repeat forever, game loop
+    while True:
+        pass # just a placeholder for now
+
+if __name__ == "__main__":
+    game_scene()
