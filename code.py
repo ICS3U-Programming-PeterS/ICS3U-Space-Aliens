fb319107b4a565b5a8156d5e2cbead794249698b
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2023-01-11 06:45:23 +0000

    Added code.py

--- code.py
+++ code.py
@@ -1,23 +1,33 @@
 #!/usr/bin/env python3
-
 # Created by: Peter Sobowale
-# Created on: January 2023
-# This program is the "Space Aliens" program on the PyBadge
-
+# Created on: Jan 2023
+# This program is for space alien in the PyBadge
 import ugame
 import stage
 
 
 def game_scene():
-    # this function is the main game screen
+    # this is the main scene for background
+    # image bank for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("ball.bmp")
 
-    print("\n\n\n")  # 3 blank lines
-    print("Hello, World(Peter)!")  # what is going to be displayed
-    print("Extra functionality")
+    # set the background to image 0 in the image bank
+    # and the size
+    background = stage.Grid(image_bank_background, 10, 8)
 
-    # repeat forever, game loop
+    # create a stage for the background to show up on
+    # set the frame rate to 60 fps
+    game = stage.Stage(ugame.display, 60)
+
+    # set the layers of all the sprites, items show up in order
+    game.layers = [background]
+
+    # render all sprites
+    game.render_block()
+
+    # repeat forever game loop
     while True:
-        pass  # just a placeholder for now
+        pass
 
 
 if __name__ == "__main__":
