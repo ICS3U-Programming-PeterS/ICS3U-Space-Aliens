3b47aa261f2f000c4bb787b15191a31ecdfa2e9a
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2023-01-12 18:33:46 +0000

    Added code.py

--- code.py
+++ code.py
@@ -1,34 +1,51 @@
 #!/usr/bin/env python3
+ 
 # Created by: Peter Sobowale
 # Created on: Jan 2023
-# This program is for space alien in the PyBadge
-import ugame
+# This program is the "Space Aliens" game for the PyBadge.
+# It uses the ugame and stage libraries to manage the graphics
+# and input for the game.
+ 
 import stage
-
-
+import ugame
+ 
+ 
 def game_scene():
-    # this is the main scene for background
-    # image bank for CircuitPython
-    image_bank_background = stage.Bank.from_bmp16("ball.bmp")
-
-    # set the background to image 0 in the image bank
-    # and the size
+    # This function sets up and runs the main game scene.
+ 
+    # Load the background and sprite image banks
+    image_bank_background = stage.Bank.from_bmp16("pingpong_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+ 
+    # Create the background grid using the image and set the size to 10x8 tiles
     background = stage.Grid(image_bank_background, 10, 8)
+ 
+    # Create the ship sprite using image at index 5, with initial position
+    # (72,57)
+    ship = stage.Sprite(image_bank_sprites, 5, 72, 57)
 
-    # create a stage for the background to show up on
-    # set the frame rate to 60 fps
+    # Create the ship sprite using image at index 5, with initial position
+    # (72,57)
+    table = stage.Sprite(image_bank_sprites, 1, 0, 50)
+ 
+    # Create a "Stage" object to manage the game graphics and input
+    # Set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
-
-    # set the layers of all the sprites, items show up in order
-    game.layers = [background]
-
-    # render all sprites
+ 
+    # Add the background and ship to the layers list
+    game.layers = [ship, table] + [background]
+ 
+    # Draw the background on the screen
     game.render_block()
-
-    # repeat forever game loop
+ 
+    # Game Loop
     while True:
         pass
-
-
+        # Redraw the sprites on the screen
+        game.render_sprites([ship, table])
+        # Pause the loop to achieve 60fps frame rate
+        game.tick()
+ 
+ 
 if __name__ == "__main__":
     game_scene()
