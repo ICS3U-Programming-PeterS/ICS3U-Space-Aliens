91d616a728c876e9e5d5435e9a9dc8f455cddd0b
Peter Sobowale <peter.sobowale@stu.ocsb.ca>
2023-01-17 08:54:48 +0000

    Added code.py

--- code.py
+++ code.py
@@ -5,8 +5,78 @@
 # This program is the "Space Aliens" game for the PyBadge.
 import stage
 import ugame
-
+import time
+import random
 import constants
+
+
+def splash_scene():
+    # This function sets up and runs the splash scene.
+
+    # get coin sound ready
+    coin_sound = open("coin.wav", "rb")
+    sound = ugame.audio
+    sound.stop()
+    sound.mute(False)
+
+    # play coin sound
+    sound.play(coin_sound)
+
+    # Load the background and sprite image banks
+    image_bank_background = stage.Bank.from_bmp16("mt_game_studio.bmp")
+
+    # Create the background grid using the image and set the size to 10x8 tiles
+    background = stage.Grid(
+        image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
+    )
+
+    # used this program to split the image into tile:
+    #   https://ezgif.com/sprite-cutter/ezgif-5-818cdbcc3f66.png
+
+    background.tile(2, 2, 0)  # blank white
+    background.tile(3, 2, 1)
+    background.tile(4, 2, 2)
+    background.tile(5, 2, 3)
+    background.tile(6, 2, 4)
+    background.tile(7, 2, 0)  # blank white
+
+    background.tile(2, 3, 0)  # blank white
+    background.tile(3, 3, 5)
+    background.tile(4, 3, 6)
+    background.tile(5, 3, 7)
+    background.tile(6, 3, 8)
+    background.tile(7, 3, 0)  # blank white
+
+    background.tile(2, 4, 0)  # blank white
+    background.tile(3, 4, 9)
+    background.tile(4, 4, 10)
+    background.tile(5, 4, 11)
+    background.tile(6, 4, 12)
+    background.tile(7, 4, 0)  # blank white
+
+    background.tile(2, 5, 0)  # blank white
+    background.tile(3, 5, 0)
+    background.tile(4, 5, 13)
+    background.tile(5, 5, 14)
+    background.tile(6, 5, 0)
+    background.tile(7, 5, 0)  # blank white
+
+    # Create a "Stage" object to manage the game graphics and input
+    # Set the frame rate to 60fps
+    game = stage.Stage(ugame.display, constants.FPS)
+
+    # Add the background and ball to the layers list
+    game.layers = [background]
+
+    # Draw the background on the screen
+    game.render_block()
+
+    # repeat forever game loop
+    while True:
+        # wait for 2 seconds
+        time.sleep(2)
+        # go to the menu scene
+        menu_scene()
 
 
 def menu_scene():
@@ -27,11 +97,11 @@ def menu_scene():
         buffer=None,
     )
 
-    # Move the text to the position (20, 10)
-    text1.move(25, 10)
+    # Move the text to the position (17, 10)
+    text1.move(17, 20)
 
-    # Set the text to "MT Game Studio"
-    text1.text("Sobowale Studio")
+    # Set the text to "Ping Pong Escape"
+    text1.text("Ping Pong Escape")
 
     # Add the text object to the text list
     text.append(text1)
@@ -90,7 +160,7 @@ def game_scene():
 
     # get sound file ready.
     pew_sound = open("pew.wav", "rb")
-    coin_sound = open("pingpong.wav", "rb")
+    pingpong_sound = open("pingpong.wav", "rb")
     sound = ugame.audio
     sound.stop()
     sound.mute(False)
@@ -99,6 +169,13 @@ def game_scene():
     background = stage.Grid(
         image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y
     )
+
+    # set the background tile to choose random background from 1 - 3
+    # Do this using nested for each loops
+    for x_location in range(constants.SCREEN_GRID_X):
+        for y_location in range(constants.SCREEN_GRID_Y):
+            tile_picked = random.randint(1, 3)
+            background.tile(x_location, y_location, tile_picked)
 
     # Create the ball sprite using image at index 5, with initial position
     # (56,57)
@@ -146,6 +223,7 @@ def game_scene():
             else:
                 a_button = constants.button_state["button_up"]
 
+        # B button to fire
         if keys & ugame.K_X != 0:
             if b_button == constants.button_state["button_up"]:
                 b_button = constants.button_state["button_just_pressed"]
@@ -157,11 +235,13 @@ def game_scene():
             else:
                 b_button = constants.button_state["button_up"]
 
+        # if they press start
         if keys & ugame.K_START:
             pass
 
+        # if they press the select button
         if keys & ugame.K_SELECT:
-            pass
+            menu_scene()
 
         # code to move ball sprite and to rap it
         if keys & ugame.K_RIGHT != 0:
@@ -170,18 +250,21 @@ def game_scene():
             else:
                 ball.move(constants.SCREEN_X - constants.SPRITE_SIZE, ball.y)
 
+        # if they press the left button
         if keys & ugame.K_LEFT != 0:
             if ball.x >= 0:
                 ball.move(ball.x - 1, ball.y)
             else:
                 ball.move(0, ball.y)
 
+        # if they press the button to move up
         if keys & ugame.K_UP != 0:
             if ball.y >= 0:
                 ball.move(ball.x, ball.y - 1)
             else:
                 ball.move(ball.x, 120)
 
+        # if they press the button to move down
         if keys & ugame.K_DOWN != 0:
             if ball.y <= 120:
                 ball.move(ball.x, ball.y + 1)
@@ -193,7 +276,7 @@ def game_scene():
         if a_button == constants.button_state["button_just_pressed"]:
             sound.play(pew_sound)
         if b_button == constants.button_state["button_just_pressed"]:
-            sound.play(coin_sound)
+            sound.play(pingpong_sound)
 
         # Redraw the Sprites
         game.render_sprites([ball] + [table, table2, alien])
@@ -203,4 +286,4 @@ def game_scene():
 
 
 if __name__ == "__main__":
-    menu_scene()
+    splash_scene()
